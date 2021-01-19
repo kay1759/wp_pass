28a532e8527a52eb37b009ea0867783dfc623c79
Katsuyoshi Yabe <kay@wizis.com>
2021-01-19 12:54:14 +1100

    Register for hex.pm

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
 # WPPass
 
-WprdPress Password Check for elixir
+WordPress Password Check for elixir
 
 Password generator is not included
+
+## Installation
+
+The package can be installed by adding `:wp_pass` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:wp_pass, "~> 0.1.0"}
+  ]
+end
+```
 
 ## Operating Instructions:
 ### Usage:
     WPPass.check_password(<plain_password>, <stored_password>)
 	
 return Boolean (true or flase)
+
+### convert Authentication Logic on Phoenix
+
+When login, user input the plain password.
+```
+if Bcrypt checkpass OK
+      login
+else
+	if WordPress checkpass OK
+		Save encrypted password using Bcrypt
+		Login
+	else
+		Authentication Failure
+```
 
 ## Test:
 ### Unit Test:
--- mix.exs
+++ mix.exs
@@ -1,16 +1,31 @@
 defmodule WPPass.MixProject do
   use Mix.Project
 
+  @description"""
+  WordPress Password Check for elixir
+  """
+  
   def project do
     [
       app: :wp_pass,
       version: "0.1.0",
       elixir: "~> 1.11",
+      name: "WPPass",
+      description: @description,
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
+  def package do
+    [
+      maintainers: ["Katsuyoshi Yabe"],
+      licenses: ["MIT"],
+      links: %{ "Github": "https://github.com/kay1759/wp_pass" }
+    ]
+  end
+  
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
@@ -21,7 +36,8 @@ defmodule WPPass.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      { :white_bread, "~> 4.5", only: [:dev, :test] }
+      { :white_bread, "~> 4.5", only: [:dev, :test] },
+      { :ex_doc, "~> 0.19.3", only: :dev, runtime: false}
        # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
